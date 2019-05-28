a2487dda0bc054aa2903c54f1611041418b73103
Phil Snyder <subfivemile@gmail.com>
2019-05-28 15:48:18 -0700

    bookmark

--- curate_clinic_motor_tasks.py
+++ curate_clinic_motor_tasks.py
@@ -1,10 +1,11 @@
 '''
 Tidy and push table `SCORES` to Synapse as a Table object, but with additional
-columns `sensor` for sensor type, `location` for sensor location, and
-`sensor_data` containing the sensor measurements which correspond to the task
-being performed.
+columns `device` for device, `sensor_type` for sensor type, `sensor_location`
+for sensor location, and `sensor_data` containing the sensor measurements which
+correspond to the task being performed.
 '''
 
+import os
 import argparse
 import multiprocessing
 import synapseclient as sc
@@ -12,8 +13,10 @@ import synapseutils as su
 import pandas as pd
 
 SCORES = "syn18435302"
-MC_10_MEASUREMENTS = "syn18822536" #"syn18435632"
+MC10_MEASUREMENTS = "syn18822536" #"syn18435632"
 SMARTWATCH_MEASUREMENTS = "syn18822537" #"syn18435623"
+SMARTWATCH_SENSOR_NAME = "smartwatch"
+MC10_SENSOR_NAME = "mc10"
 TASK_CODE_MAP = { # synchronize with MJFF Levodopa release
         "Drnkg": "drnkg",
         "Drwg": "drwg",
@@ -46,20 +49,82 @@ def read_syn_table(syn, synapse_id, q = "select * from {}"):
     return df
 
 
-def download_children(syn, parent, download_in_parallel=False):
-    _, _, entity_info = list(su.walk(syn, parent))[0]
-    if download_in_parallel:
-        synapse_ids = [j for i, j in entity_info]
-        mp = multiprocessing.dummy.Pool(4)
-        entities = mp.map(syn.get, synapse_ids)
-        children = {entity_info[i][0]: entities[i] for i in range(len(entities))}
+def parse_info_from_filename(fname, sensor):
+    if sensor == SMARTWATCH_SENSOR_NAME:
+        _, subject_id, year_month = os.path.splitext(fname)[0].split("_")
+        year, month = tuple(map(int, year_month.split("-")))
+    elif sensor == MC10_SENSOR_NAME:
+        subject_id = int(os.path.splitext(fname)[0].split("_")[1])
+        year, month = None, None
     else:
-        children = {i: syn.get(j) for i, j in entity_info}
-    return children
+        raise TypeError("sensor must be one of {} or {}".format(
+            SMARTWATCH_SENSOR_NAME, MC10_SENSOR_NAME))
+    return subject_id, year, month
+
+
+def find_relevant_scores(fname, scores, sensor):
+    def is_match(t, year, month, f_subject_id, score_subject_id):
+        if int(f_subject_id) == int(score_subject_id):
+            if year is None or month is None:
+                return True
+            elif isinstance(year, int) and isinstance(month, int):
+                return year == t.year and month == t.month
+            else:
+                raise TypeError("Year and month must both be integers.")
+        else:
+            return False
+    subject_id, year, month = parse_info_from_filename(fname, sensor)
+    relevent_scores = scores.apply(
+            lambda s : is_match(s.start_utc, year, month, subject_id, s.subject_id),
+            axis = 1)
+    return scores[relevent_scores]
+
+def get_measurement_type(fname):
+    if fname.startswith("Table9A") or fname.startswith("Table8"):
+        return "accelerometer"
+    elif fname.startswith("Table9B"):
+        return "gyroscope"
+    elif fname.startwith("Table9C"):
+        return "emg"
+    else:
+        return None
+
+def download_relevant_children(syn, parent, scores, sensor,
+                               download_in_parallel=False):
+    """
+    Returns
+    -------
+    1. dataframe of mapping Synapse ID to relevant scores (pandas DataFrame)
+    2. dictionary mapping synapse ID to Synapse File object
+    """
+    _, _, entity_info = list(su.walk(syn, parent))[0]
+    relevant_score_rows = {}
+    relevant_entities = []
+    for fname, syn_id in entity_info:
+        measurement_type = get_measurement_type(fname)
+        relevant_scores = find_relevant_scores(fname, scores, sensor)
+        if len(relevant_scores):
+            relevant_scores["device"] = sensor
+            relevant_scores["sensor_type"] = measurement_type
+            relevant_score_rows[syn_id] = relevant_scores
+            relevant_entities.append(syn_id)
+    if download_in_parallel:
+        mp = multiprocessing.dummy.Pool(4)
+        children = mp.map(syn.get, relevant_entities)
+    else:
+        children = list(map(syn.get, relevant_entities))
+    id_to_file_mapping = {k: v for k, v in zip(relevant_entities, children)}
+    return relevant_score_rows, id_to_file_mapping
 
 
 def slice_from_score(sensor_measurement, score):
-    result = {}
+    """
+    Returns
+    -------
+    a dict mapping location of sensor to a pandas DataFrame with
+    x, y, z measurements
+    """
+    result = pd.DataFrame(columns = ["sensor_location", "sensor_data"])
     start, stop = score.loc["start_utc"], score.loc["stop_utc"]
     relevant_range = sensor_measurement[start:stop]
     for location in relevant_range.Location.unique():
@@ -69,7 +134,10 @@ def slice_from_score(sensor_measurement, score):
         time_zero = local_range.Timestamp.iloc[0]
         local_range.Timestamp = local_range.Timestamp - time_zero
         location = "_".join(location.split())
-        result[location] = local_range
+        result = result.append({"sensor_location": location,
+                                "sensor_data": local_range},
+                               ignore_index=True)
+    result.index = pd.Index([score.name] * len(result))
     return result
 
 
@@ -79,15 +147,21 @@ def slice_sensor_measurement(f, scores):
     sensor_measurement.set_index("Timestamp", drop = True, inplace=True)
     sensor_measurement.sort_index(inplace=True)
     relevant_scores = filter_relevant_scores(sensor_measurement, scores)
-    results = relevant_scores.apply(
+    measurements = relevant_scores.apply(
             lambda score : slice_from_score(sensor_measurement, score),
             axis = 1)
-    return(results)
+    measurements = pd.concat(measurements.values, axis=0)
+    merged_results = pd.merge(relevant_scores, measurements,
+                              left_index=True, right_index=True)
+    return(merged_results)
 
+def replace_dataframe_with_filehandle(df):
+    pass
 
-def slice_files(files, sensor):
-    for fname, f in files.items():
-
+def slice_files(score_rows, files):
+    for synapse_id, f in files.items():
+        slices = slice_sensor_measurement(f, score_rows[synapse_id])
+    return slices
 
 
 def filter_relevant_scores(sensor_measurement, scores):
@@ -139,17 +213,17 @@ def clean_scores(scores):
 
 
 def main():
-    # TODO: only download relevant files (those present in scores)
-    #       during download_children by matching subject_id values
-    #       in filename. Copy some sample files over to test folders
-    #       for testing
     args = read_args()
     syn = sc.login()
     scores = clean_scores(read_syn_table(syn, SCORES))
-    smartwatch_files = download_children(syn, SMARTWATCH_MEASUREMENTS,
-                                         args.download_in_parallel)
-    mc10_files = download_children(syn, MC_10_MEASUREMENTS,
-                                      args.download_in_parallel)
+    smartwatch_rows, smartwatch_files = download_relevant_children(
+            syn, SMARTWATCH_MEASUREMENTS, scores,
+            args.download_in_parallel, sensor = SMARTWATCH_SENSOR_NAME)
+    mc10_rows, mc10_files = download_relevant_children(
+            syn, MC_10_MEASUREMENTS, scores,
+            args.download_in_parallel, sensor = MC10_SENSOR_NAME)
+    sliced_smartwatch_rows = slice_files(smartwatch_rows, smartwatch_files)
+    sliced_mc10_rows = slice_files(mc10_rows, mc10_files)
 
 
 if __name__ == "__main__":
