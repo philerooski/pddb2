6476f6f5501d8b0f38c2ac625cf6a6beb7e7d152
Phil Snyder <subfivemile@gmail.com>
2019-09-26 15:49:58 -0700

    cache sliced files and do not recreate extant slices

--- curate_real_pd.R
+++ curate_real_pd.R
@@ -3,11 +3,12 @@ library(tidyverse)
 library(furrr)
 
 TESTING = FALSE
-PARALLEL_WORKERS = 6
+PARALLEL_WORKERS = 2
 CACHE_DIR = if (TESTING) "cache" else "/root/cache"
 DIARY = "syn20769648"
 SENSOR_START_TIMES <- "syn20712822"
 SENSOR_DATA <- "syn20542701"
+SENSOR_DATA_REFERENCE <- "syn20820314"
 OUTPUT_PROJECT <- if (TESTING) "syn11611056" else "syn18407520"
 
 fetch_diary <- function() {
@@ -82,10 +83,64 @@ fetch_sensor_data <- function() {
   return(sensor_data)
 }
 
+get_sensor_data_reference <- function() {
+  sensor_data_ref <- as_tibble(synTableQuery(
+    paste("SELECT * FROM", SENSOR_DATA_REFERENCE))$asDataFrame()) %>%
+    select(subject_id, context, device, measurement, file_identifier) %>%
+    mutate(cache_dir = as.character(fs::path(CACHE_DIR, file_identifier)))
+  return(sensor_data_ref)
+}
+
+get_already_processed_sensor_data <- function(sensor_data, sensor_data_ref) {
+  already_processed_sensor_data <- left_join(sensor_data, sensor_data_ref) %>%
+    filter(dir.exists(cache_dir)) %>%
+    select(subject_id, context, device, measurement, file_identifier, cache_dir)
+  if (nrow(already_processed_sensor_data)) {
+    sensor_data_processed <- already_processed_sensor_data %>%
+      purrr::pmap_dfr(function(subject_id, context, device, measurement, file_identifier, cache_dir) {
+        sliced_files <- dir(cache_dir)
+        if (length(sliced_files)) {
+          measurement_ids <- unlist(
+            purrr::map(sliced_files, ~ stringr::str_split(., "\\.")[[1]][[1]]))
+          paths <- unlist(
+            purrr::map(sliced_files, ~ as.character(fs::path(cache_dir, .))))
+          relevant_slices <- tibble(
+            measurement_id = measurement_ids,
+            subject_id = subject_id,
+            context = context,
+            device = device,
+            measurement = measurement,
+            file_identifier = file_identifier,
+            cache_path = paths)
+        } else {
+          relevant_slices <- tibble(
+            measurement_id = NA,
+            subject_id = NA,
+            context = NA,
+            device = NA,
+            measurement = NA,
+            file_identifier = file_identifier,
+            cache_path = NA)
+        }
+        return(relevant_slices)
+      })
+  } else {
+    sensor_data_processed <- tibble(
+      measurement_id = character(),
+      subject_id = character(),
+      context = character(),
+      device = character(),
+      measurement = character(),
+      file_identifier = character(),
+      cache_path = character())
+  }
+  return(sensor_data_processed)
+}
+
 slice_sensor_data <- function(sensor_data, diary) {
   sliced_data <- furrr::future_pmap_dfr(sensor_data, function(subject_id, context, device,
-                                                    measurement, path, first_time,
-                                                    last_time) {
+                                                    measurement, first_time, last_time,
+                                                    path, cache_dir) {
     relevant_slices <- tryCatch({
       sensor_data_df <- read_csv(path) %>%
         filter(!duplicated(time)) %>%
@@ -120,19 +175,24 @@ slice_sensor_data <- function(sensor_data, diary) {
           measurement = measurement,
           data = slices) %>%
           filter(purrr::map(data, nrow) > 0) %>%
-          mutate(measurement_id = unlist(purrr::map(data, ~ .$measurement_id[[1]])),
-                 path = purrr::map(data, function(df) {
-                   fname = tempfile(tmpdir = CACHE_DIR, fileext = ".csv")
-                   df <- df %>%
-                     select(t, x, y, z) %>%
-                     write_csv(fname)
+          mutate(measurement_id = unlist(purrr::map(data, ~ .$measurement_id[[1]])))
+        paths <- unlist(purrr::map(relevant_slices$measurement_id, function(measurement_id) {
+                   fname <- as.character(
+                     fs::path(cache_dir, paste0(measurement_id, ".csv")))
                    return(fname)
-                 })) %>%
-          select(measurement_id, subject_id, context, device, measurement, path)
-        rm(sensor_data_df)
-        rm(slices)
+         }))
+        relevant_slices$cache_path <- paths
+        dir.create(cache_dir)
+        purrr::map2(relevant_slices$data, relevant_slices$cache_path, function(df, path) {
+           df <- df %>%
+             select(t, x, y, z) %>%
+             write_csv(path)
+        })
+        relevant_slices <- relevant_slices %>%
+          select(measurement_id, subject_id, context, device, measurement, cache_path)
         return(relevant_slices)
       } else {
+        dir.create(as.character(cache_dir))
         return(tibble())
       }
     }, error = function(e) {
@@ -146,14 +206,13 @@ slice_sensor_data <- function(sensor_data, diary) {
       return(relevant_slices)
     })
     return(relevant_slices)
-  })
+  }, .options = future_options(scheduling = Inf))
   return(sliced_data)
 }
 
 replace_paths_with_filehandles <- function(list_of_paths) {
   file_handles <- purrr::map(list_of_paths, function(p) {
     fh <- synUploadSynapseManagedFileHandle(p, mimetype="text/csv")
-    unlink(p)
     return(fh$id)
   })
   file_handles <- unlist(file_handles)
@@ -167,11 +226,11 @@ store_sliced_data_and_diary <- function(sliced_data, diary, parent) {
     left_join(context, by = "measurement_id") %>%
     select(measurement_id, subject_id = diary_subject_id, context, dplyr::everything())
   data_df <- sliced_data %>%
-    select(measurement_id, subject_id, device, measurement, data)
+    select(measurement_id, subject_id, device, measurement, cache_path)
   data_df <- data_df %>%
     sample_frac(1) %>%
-    mutate(data_file_handle_id = replace_paths_with_filehandles(path)) %>%
-    select(-path) %>%
+    mutate(data_file_handle_id = replace_paths_with_filehandles(cache_path)) %>%
+    select(-cache_path) %>%
     arrange(subject_id, device, measurement, measurement_id)
   data_fname <- "real_pd_sensor_data.csv"
   write_csv(data_df, data_fname)
@@ -211,9 +270,22 @@ main <- function() {
   sensor_data <- fetch_sensor_data() %>%
     inner_join(start_times, by = c("subject_id", "device", "measurement")) %>%
     select(-id)
+  sensor_data_ref <- get_sensor_data_reference()
+  already_processed_sensor_data <- get_already_processed_sensor_data(
+    sensor_data, sensor_data_ref)
+  sensor_data <- sensor_data %>%
+    left_join(sensor_data_ref) %>%
+    anti_join(already_processed_sensor_data, by = "file_identifier") %>%
+    select(subject_id, context, device, measurement, first_time, last_time, path, cache_dir)
+  already_processed_sensor_data <- already_processed_sensor_data %>%
+    filter(!is.na(cache_path)) %>%
+    select(-file_identifier)
   plan(multiprocess, workers = PARALLEL_WORKERS)
-  sliced_data <- slice_sensor_data(sensor_data, diary)
-  store_sliced_data_and_diary(sliced_data, diary, parent = OUTPUT_PROJECT)
+  if (nrow(sensor_data)) { # if there are no rows, we have already sliced up all files
+    sliced_data <- slice_sensor_data(sensor_data, diary)
+    sliced_data <- bind_rows(sliced_data, already_processed_sensor_data)
+    store_sliced_data_and_diary(sliced_data, diary, parent = OUTPUT_PROJECT)
+  }
 }
 
 main()
